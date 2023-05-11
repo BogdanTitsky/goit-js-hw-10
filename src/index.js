df700c766651bf4aac10f57f065a1ae7648c170a
unknown <bogdan.titsky@gmail.com>
2023-05-11 13:37:14 +0300

    error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
+import fetchCountries from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+
+const searchInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+
+searchInput.addEventListener(
+  'input',
+  debounce(() => {
+    const name = searchInput.value;
+
+    fetchCountries(name)
+      .then(data => {
+        const counties = data
+          .map(country => {
+            return `<li>${country.name.official}</li>`;
+          })
+          .join('');
+
+        countryList.innerHTML = counties;
+      })
+      .catch(error => console.error(error));
+  }),
+  DEBOUNCE_DELAY
+);
