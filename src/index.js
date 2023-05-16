0482d35fd1772560ed1cf767805a4e094270c3ad
unknown <bogdan.titsky@gmail.com>
2023-05-16 09:16:59 +0300

    debounce don`t work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import fetchCountries from './js/fetchCountries';
+// import fetchCountries from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 
 const searchInput = document.querySelector('#search-box');
@@ -8,22 +8,63 @@ const countryInfo = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
 
-searchInput.addEventListener(
-  'input',
-  debounce(() => {
-    const name = searchInput.value;
+searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-    fetchCountries(name)
-      .then(data => {
-        const counties = data
-          .map(country => {
-            return `<li>${country.name.official}</li>`;
-          })
-          .join('');
+function onSearch(e) {
+  const inputValue = e.currentTarget.value;
+  getCountries(inputValue)
+    .then(data => (countryList.innerHTML = createMarkup(data)))
+    .catch(err => console.log(err));
+}
 
-        countryList.innerHTML = counties;
-      })
-      .catch(error => console.error(error));
-  }),
-  DEBOUNCE_DELAY
-);
+function getCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    return response.json();
+  });
+}
+
+function createMarkup(arr) {
+  if (arr.length >= 2 && arr.length <= 10) {
+    return arr
+      .map(
+        ({
+          name: { official },
+          flags: { svg, alt },
+        }) => `<li class="country-item">
+        <div>
+          <img class="flag" src="${svg}" alt="${alt}" />
+          <h2>${official}</h2>
+        </div>
+      </li>`
+      )
+      .join('');
+  } else {
+    return arr
+      .map(
+        ({
+          name: { official },
+          flags: { svg, alt },
+          capital,
+          languages,
+          population,
+        }) => `<li class="country-item">
+        <div>
+          <img class="flag" src="${svg}" alt="${alt}" />
+          <h2>${official}</h2>
+        </div>
+        <p><span>capital: </span> ${capital}</p>
+        <p><span>population: </span>${population}</p>
+        <p><span>languages: </span>${Object.values(languages).join(', ')}</p>
+      </li>`
+      )
+      .join('');
+  }
+}
+
+// getCountries('deutschland');
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,12 +1,1 @@
-const fetchCountries = name => {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-};
-
 export default fetchCountries;
